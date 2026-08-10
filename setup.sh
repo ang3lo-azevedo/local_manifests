2f90daca66a82f62083bd18bce63e95fd846a9dc
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-08-10 20:06:09 +0100

    Fix script(1): use args form (no -c quoting)

--- setup.sh
+++ setup.sh
@@ -88,7 +88,7 @@ if [ ! -f .repo/manifest.xml ]; then
     git clone -b 17 https://github.com/ang3lo-azevedo/voltageos-spacewar.git .repo/local_manifests
 fi
 echo "Syncing. This takes a while."
-script -q -c "script -q -c "repo sync -c -j$(nproc) --no-clone-bundle --no-tags --optimized-fetch --prune" /dev/null" /dev/null
+script -q /dev/null repo sync -c -j$(nproc) --no-clone-bundle --no-tags --optimized-fetch --prune
 
 
 echo "Adding aliases..."
